be2c2f23f79d3f17e7732316893f6e1673cb7c86
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-01-15 12:33:14 +0100

    Update dns-sync.sh

--- dns-sync.sh
+++ dns-sync.sh
@@ -8,6 +8,10 @@ dsource="mysql -s -u slavens dbispconfig -e \"select origin from dns_soa;\""
 
 domain_list=`ssh $sshurl "$dsource |sed -e 's/\.$//'"`
 
+named_conf_dir=/etc/named
+
+slave_zones_file=$master_dns-slave-zones
+
 
 current_domain_list=$(cat /tmp/domain_list |sha1sum)
 new_domain_list=$(echo $domain_list |sha1sum)
@@ -17,12 +21,12 @@ new_domain_list=$(echo $domain_list |sha1sum)
         then
         	echo "There is a new list of domains."
         	echo $domain_list > /tmp/domain_list
-        	cp /dev/null /etc/named/slave-zones
+        	cp /dev/null $named_conf_dir/$slave_zones_file
 
             for domain in $domain_list
             do
 
-                echo "zone \"$domain\" IN { type slave; file \"slaves/$domain\"; masters { $master_dns; }; }; " >> /etc/named/slave-zones
+                echo "zone \"$domain\" IN { type slave; file \"slaves/$domain\"; masters { $master_dns; }; }; " >> $named_conf_dir/$slave_zones_file
             done
                 echo "I am restarting the dns server."
                 systemctl restart named
